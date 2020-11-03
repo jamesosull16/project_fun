c74be98eadf33d7bd1fc247ef8e4910eccf8ab74
jamesosull16 <jamesosull16@gmail.com>
2020-11-03 16:01:29 -0700

    work on comparing dates and appending future holidays

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 $(document).ready(function () {
   var getHolidays = function () {
-    var requestUrl = `https://calendarific.com/api/v2/holidays?&country=US&year=2020&type=national&api_key=243b50890c210a5cfad34ab2a2a57915a0dc10bb`;
+    var currentDate = new Date();
+    var dd = currentDate.getDate();
+    var mm = currentDate.getMonth();
+    var yyyy = currentDate.getFullYear();
+    if (dd < 10) {
+      dd = "0" + dd;
+    }
+    if (mm < 10) {
+      mm = 0 + mm;
+    }
+    currentDate = mm + "/" + dd + "/" + yyyy;
+
+    var requestUrl = `https://holidays.abstractapi.com/v1/?api_key=7639aa293d574a51bbeed5c5a63d90ac&country=US&year=${yyyy}`;
     //ajax call
     $.ajax({
       url: requestUrl,
@@ -12,15 +24,11 @@ $(document).ready(function () {
         var holidayName = $("<h3>");
         var holidayDate = $("<h3>");
 
-        var currentDate = moment().format("MM/DD/YYYY");
-
-        holidayName.text("Holiday: " + data.response.holidays[i].name);
-        holidayDate.text(
-          "Date: " +
-            moment(data.response.holidays[i].date.iso).format("MM/DD/YYYY")
-        );
+        holidayName.text("Holiday: " + data[i].name);
+        holidayDate.text("Date: " + data[i].date);
 
         if (holidayDate > currentDate) {
+          // if (new Date(holidayDate).getTime() > new Date(currentDate).getTime()) {
           holiday.append(holidayName);
           holiday.append(holidayDate);
         }
